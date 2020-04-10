d8e4725119c464cfb45dc218536ead4d0b1c7c62
tungtx88 <taxuantung.88@gmail.com>
2020-04-10 15:29:21 +0700

    update

--- components/crud/Tag.js
+++ components/crud/Tag.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 import {isAuth, getCookie} from '../../actions/auth'
-import {create, getTags, singleTag, removeTag} from '../../actions/Tag'
+import {create, getTags, singleTag, removeTag} from '../../actions/tag'
 
 const Tag = () => {
     const [values, setValues] = useState({
